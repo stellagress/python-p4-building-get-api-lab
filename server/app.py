c8d80706f2510dc33e228ccd19fe29a1992c2087
Stella Mazzillo <stellagress5@gmail.com>
2023-09-05 09:39:44 -0400

    complete p4 building get api

--- server/app.py
+++ server/app.py
@@ -13,6 +13,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
+# migrate = Migrate(app, db, render_as_batch=True)
 
 db.init_app(app)
 
@@ -58,6 +59,21 @@ def bakery_by_id(id):
         200
     )
     return response
+
+
+# @app.route('/bakeries/<int:id>')
+# def bakery_by_id(id):
+#     bake = Bakery.query.filter(Bakery.id == id).first()
+    
+#     bake_dict = bake.to_dict()
+ 
+
+#     response = make_response(
+#         bake_dict,
+#         200
+#     )
+#     return response
+
 
 
 
@@ -125,11 +141,95 @@ def most_expensive_baked_good():
     return response
 
 
-
-
-
-
-
-
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+
+# #!/usr/bin/env python3
+
+# from flask import Flask, make_response, jsonify
+# from flask_migrate import Migrate
+
+# from models import db, Bakery, BakedGood
+# import json
+
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.json.compact = True
+
+# migrate = Migrate(app, db)
+
+# db.init_app(app)
+
+# @app.route('/')
+# def index():
+#     return '<h1>Bakery GET API</h1>'
+
+# @app.route('/bakeries')
+# def bakeries():
+#     bakery = []
+#     for baker in Bakery.query.all():
+
+#         bakery_dic = baker.to_dict()
+#         bakery.append(bakery_dic)
+    
+#     #make json more readable 
+#     json_str = json.dumps(bakery, indent=4)
+#     response = make_response(json_str,200)
+
+#     response.headers["Content-Type"] = "application/json"
+   
+#     return response
+
+# @app.route('/bakeries/<int:id>')
+# def bakery_by_id(id):
+#     bakery = Bakery.query.filter(Bakery.id == id).first()
+    
+#     bakery_dict = bakery.to_dict()
+
+#     json_str = json.dumps(bakery_dict, indent=4)
+    
+#     response = make_response(json_str, 200)
+
+#     response.headers["Content-Type"] = "application/json"
+
+#     return response
+
+# @app.route('/baked_goods/by_price')
+# def baked_goods_by_price():
+
+#     baked_list = []
+
+#     baked_goods =  BakedGood.query.order_by(BakedGood.price).all() 
+      
+#     for baked in baked_goods:  
+
+#         baked_dic = baked.to_dict()
+#         baked_list.append(baked_dic)
+
+#     json_str = json.dumps(baked_list, indent=4)
+   
+#     response = make_response(json_str, 200)
+
+#     response.headers["Content-Type"] = "application/json" 
+
+#     return response
+
+# @app.route('/baked_goods/most_expensive')
+# def most_expensive_baked_good():
+     
+#     baked_goods =  BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first() 
+  
+#     baked_dict = baked_goods.to_dict()
+
+#     json_str = json.dumps(baked_dict, indent=4)
+
+#     response = make_response(json_str, 200)
+
+#     response.headers["Content-Type"] = "application/json" 
+    
+#     return response
+
+# if __name__ == '__main__':
+#     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,10 +1,23 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
+
+# metadata = MetaData(
+#     naming_convention={
+#         "ix": "ix_%(column_0_label)s",
+#         "uq": "uq_%(table_name)s_%(column_0_name)s",
+#         "ck": "ck_%(table_name)s_%(column_0_name)s",
+#         "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+#         "pk": "pk_%(table_name)s",
+#      }
+# )
+
+
 
 db = SQLAlchemy(metadata=metadata)
 
@@ -12,7 +25,7 @@ db = SQLAlchemy(metadata=metadata)
 
 class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
-    serialize_rules = ('-baked_goods')
+    serialize_rules = ('-baked_goods.bakery',)
 
     id = db.Column(db.Integer, primary_key=True)
 
@@ -28,7 +41,7 @@ class Bakery(db.Model, SerializerMixin):
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
-    serialize_rules = ('-bakery.baked_goods')
+    serialize_rules = ('-bakery.baked_goods',)
 
     id = db.Column(db.Integer, primary_key=True)
     
@@ -43,3 +56,55 @@ class BakedGood(db.Model, SerializerMixin):
         # return f'<BakedGood id={self.id} name="{self.name}">'
         return f'<BakedGood id={self.id} name="{self.name}">'
     
+
+
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy import MetaData
+# from sqlalchemy_serializer import SerializerMixin
+
+# # metadata = MetaData(naming_convention={
+# #     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+# # })
+
+# metadata = MetaData(
+#     naming_convention={
+#         "ix": "ix_%(column_0_label)s",
+#         "uq": "uq_%(table_name)s_%(column_0_name)s",
+#         "ck": "ck_%(table_name)s_%(column_0_name)s",
+#         "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+#         "pk": "pk_%(table_name)s",
+#      }
+# )
+
+# db = SQLAlchemy(metadata=metadata)
+
+# class Bakery(db.Model, SerializerMixin):
+#     __tablename__ = 'bakeries'
+
+#     serialize_rules = ('-baked_goods.bakery',)
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String)
+#     created_at = db.Column(db.String)
+#     updated_at = db.Column(db.String)
+
+#     baked_goods = db.relationship('BakedGood', backref='bakery')
+
+#     def __repr__(self):
+#        return f'<Bakery: {self.name} was created at {self.created_at} and updated at {self.updated_at}>'
+
+# class BakedGood(db.Model, SerializerMixin):
+#     __tablename__ = 'baked_goods'
+
+#     serialize_rules = ('-bakery.baked_goods',)
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String)
+#     price = db.Column(db.Integer)
+#     created_at = db.Column(db.String)
+#     updated_at = db.Column(db.String)
+    
+#     bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
+
+#     def __repr__(self):
+#        return f'<BakedGood: {self.name} the cost is {self.price} was created at {self.created_at} and updated at {self.updated_at}>'
